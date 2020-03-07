d7cdc32918d1bd3f7b965e262b38b521a23bc3df
kitcasttest <kitcasttest@gmail.com>
2020-03-07 23:15:28 +0200

    Add Finance file, new frame

---
+++ Graphics/finance.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class Main(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Main(root)
+    app.pack()
+    root.title("Finance App")
+    root.geometry("640x480+300+200")
+    root.resizable(False, False)
+    root.mainloop()
